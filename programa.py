f928fd06a32838ba26807df6ca959f1c9cea3492
Wallace Franco <wallace.franco@outlook.com>
2024-06-27 11:57:34 -0300

    Incluindo arquivo programa.py

---
+++ programa.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+tela = tk.Tk()
+tela. title("Calculadora Simples")
+
+largura_tela = 500
+altura_tela = 500
+
+tela.geometry(f"{largura_tela}x{altura_tela}")
+tela.mainloop()
+
+print("Funcionou")
